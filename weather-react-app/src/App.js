bea44f8f1b50a05c3f0c17abe160cce708bb32c2
kostas.panagiotou <kostas.panagiotou08@gmail.com>
2022-09-12 09:14:59 +0000

    Create the react app, download react-icons packages, dwonload two images and create two files components and assets and start build up on the app.js

---
+++ weather-react-app/src/App.js
@@ -0,0 +1,35 @@
+import hotBg from "./assets/hot.jpg";
+import coldBg from "./assets/cold.jpg";
+
+function App() {
+  return (
+    <div className="app" style={{ backgroundImage: `url(${coldBg})` }}>
+     <div className="overlay">
+      <div className="container">
+        <div className="section section_inputs">
+          <input type="text" name="city" placeholder="Enter City .."/>
+          <button>°C</button>
+        </div>
+      
+        <div className="section section__temperature">
+          <div className="icon">
+            <h3>Athens, GR</h3>
+            <img src="" alt=""
+            />
+            <h3>Cloudy</h3>
+          </div>
+          <div className="temperature">
+            <h1>34 °C</h1>
+          </div>
+        </div>
+        
+      {/*bottom description*/}
+      </div>
+     </div>
+    </div> 
+ 
+ 
+  );
+}
+
+export default App;
